b909e205a4752fd0ca3505069bf98f85ab541717
K4izuo <crisjustineoracion22@gmail.com>
2025-11-19 17:17:38 +0800

    edit

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
-    AuthController,
-    UsersController,
-    AdminController,
-    CampusesController,
-    OfficesController,
-    DegreeCoursesController,
-    EmailVerificationController,
-    AssetsController,
-    ReservationController
+  AuthController,
+  UsersController,
+  // AdminController,
+  CampusesController,
+  OfficesController,
+  DegreeCoursesController,
+  EmailVerificationController,
+  AssetsController,
+  ReservationController
 };
 
 // Public routes
@@ -19,24 +19,18 @@ Route::post('users/login', [AuthController::class, 'login']);
 Route::post('users/store', [UsersController::class, 'store']);
 Route::get('verify-email', [EmailVerificationController::class, 'verify']);
 Route::post('resend-verification', [EmailVerificationController::class, 'resend'])
-    ->middleware('throttle:5,1');
+  ->middleware('throttle:5,1');
+Route::get('campuses/all', [CampusesController::class, 'index']);
+Route::get('offices/all', [OfficesController::class, 'index']);
+Route::get('degreeCourse/{id}', [DegreeCoursesController::class, 'show']);
+Route::get('assets/all', [AssetsController::class, 'index']);
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
-    // Auth
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('logout-all', [AuthController::class, 'logoutAll']);
-    Route::get('me', [AuthController::class, 'me']);
-    
-    // Resources
-    Route::get('campuses/all', [CampusesController::class, 'index']);
-    Route::get('offices/all', [OfficesController::class, 'index']);
-    Route::get('degreeCourse/{id}', [DegreeCoursesController::class, 'show']);
-    Route::get('assets/all', [AssetsController::class, 'index']);
-    
-    // Admin
-    Route::post('admin/store', [AdminController::class, 'store']);
+  // Auth
+  Route::post('logout', [AuthController::class, 'logout']);
+  Route::get('me', [AuthController::class, 'me']);
 
-    // Event Reservation
-    Route::post('event/reservation', [ReservationController::class, 'store']);
+  // Event Reservation
+  Route::post('event/reservation', [ReservationController::class, 'store']);
 });
